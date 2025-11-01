09f2ef692325b9419e039d532e80f7479cf5a0aa
Ahmed Hossny <ahmedgamalhosiny@gmail.com>
2025-11-01 17:12:50 +0200

    Updated build step to use Maven_3_9_9

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,18 +1,21 @@
 def call() {
-    echo "  Starting build process..."
+    echo "Starting build process..."
 
     stage('Checkout') {
         echo "Cloning the project repository..."
-        // Instead of 'checkout scm', do this:
+        // Clone your Git repo
         git branch: 'main', url: 'https://github.com/ahmedgamalhosiny/java-pipeline-iti.git'
     }
 
     stage('Build') {
         echo "Building Java project using Maven..."
-        bat "mvn clean package"
+        // Use the Jenkins Maven tool
+        withMaven(maven: 'Maven_3_9_9') {
+            bat "mvn clean package"
+        }
     }
 
     stage('Deploy') {
-        echo " Deployment successful!"
+        echo "Deployment successful!"
     }
 }
