c8bf383f5c5bbd561c042bce039801052bd26267
Ahmed Hossny <ahmedgamalhosiny@gmail.com>
2025-11-01 16:57:29 +0200

    Added

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,16 +1,25 @@
 def call() {
-    stage('Shared Build') {
-        echo "Running Maven build using Shared Library..."
-        bat 'mvn clean package'
+    echo " Starting build process..."
+    
+    stage('Checkout') {
+        echo "Cloning the project repository..."
+        checkout scm
     }
 
-    stage('Shared Test') {
-        echo "Running tests using Shared Library..."
-        bat 'mvn test'
+    stage('Build') {
+        echo "Building Java project using Maven..."
+        bat "mvn clean package"
     }
 
-    stage('Shared Deploy') {
-        echo "Deploying application using Shared Library..."
-        echo "Application deployed successfully (via Shared Library)!"
+    stage('Test') {
+        echo "Running unit tests..."
+        bat "mvn test"
     }
+
+    stage('Deploy') {
+        echo "Deploying application (simulation)..."
+        echo " Deployment successful!"
+    }
+    
+    echo " BuildApp pipeline finished successfully."
 }
